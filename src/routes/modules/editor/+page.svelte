8fbe468dbc9e0f609d2aa1bcc891105bf93ff168
sierra-95 <michaelmachohi@gmail.com>
2025-12-26 19:37:18 +0300

    Properly documented layout component

--- src/routes/modules/editor/+page.svelte
+++ src/routes/modules/editor/+page.svelte
@@ -22,7 +22,7 @@
 	<title>Editor</title>
 	<h1>Editor</h1>
     <Editor bind:content/>
-        <RenderCode
+    <RenderCode
 		lang="svelte"
 		code={`
 		<\script>
@@ -31,16 +31,36 @@
 		<\/script>
         <Editor bind:content/>
 	`}/>
-    <h2>Inserting Files</h2>
+    <h2>Saving & Loading Content</h2>
+    <h3>
+        After typing and clicking save, the JSON content and any images used in the editor will be stored in the <code>content</code> variable.
+    </h3>
+    <h3>
+        The <code>content</code> variable can also be used to load previously saved content. 
+        The JSON content is important because it preserves all styling and content from the editor, and can be saved in a database.
+    </h3>
+    <h2>Read only</h2>
+    <h3>To read content created in this editor, use <code>readOnly</code> mode</h3>
+    <RenderCode
+		lang="svelte"
+		code={`
+		<\script>
+            import {Editor} from '@sierra-95/svelte-ui';
+            export let data;
+            let content = data.content; // Load previously saved content
+		<\/script>
+        <Editor readOnly bind:content/>
+	`}/>
+    <h2>Inserting Images</h2>
+    <h3>A storage solution is required to store images to be used by the editor, as it only uses url's</h3>
     <h3>The editor primarily uses
         <a href={routes.file_picker} class="note">File Picker</a>
-        to handle file uploads and insertions.
-    </h3>
+        to handle image uploads and insertions.
+    If you wish to use a different storage solution, an input has been provided to key in image URLs manually.</h3>
     <h2>Storage Solutions</h2>
     <h3>The
         <a href={routes.file_picker} class="note">File Picker</a>
-        was primarily built to use Cloudflare R2 as a backend storage solution.
-        If you wish to use a different storage solution, you can ignore <code>store.r2_key</code>
+        runs on Cloudflare R2 as a backend storage solution.
     </h3>
     <RenderCode
 		lang="svelte"
