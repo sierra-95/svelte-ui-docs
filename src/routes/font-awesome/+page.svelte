0bf8b955abe775a560fca7ba3f38580434a2778a
sierra-95 <michaelmachohi@gmail.com>
2026-01-02 19:28:21 +0300

    Generated Kit Code

--- src/routes/font-awesome/+page.svelte
+++ src/routes/font-awesome/+page.svelte
@@ -9,8 +9,8 @@
   <h2>Getting Started</h2>
   <h3>Create an account
     <a href="https://fontawesome.com/" class="note" target="_blank">here</a>
-    and follow <a href="https://docs.fontawesome.com/" class="note" target="_blank">these instructions</a>
-    to make your own kit and get kit embed code.
+    and follow <a href="https://docs.fontawesome.com/" class="note" target="_blank">these</a>
+    instructions to make your own kit and get kit embed code.
   </h3>
   <h3>Add this in your <strong>app.html</strong></h3>
   <RenderCode
